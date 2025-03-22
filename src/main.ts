a9850dec3ed585a3e19b6befd075f4ca381c4bc7
Daniel Power <me@danielpower.ca>
2025-03-22 18:32:43 -0230

    Extract beatmap data and audio from osz file

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,38 @@
 import "./style.css";
 
 import { createRenderer } from "./renderer";
-import { BeatmapDecoder } from "osu-parsers";
+import { readBeatmapSet } from "./osu_files";
 
-const decoder = new BeatmapDecoder();
+window.start = async () => {
+  const { beatmaps, audios } = await readBeatmapSet("/beatmap.osz");
 
-const beatmapResponse = await fetch("/beatmap.osu");
-const beatmapText = await beatmapResponse.text();
-const beatmap = decoder.decodeFromString(beatmapText);
-const renderer = await createRenderer({ beatmap });
-
-const update = () => {
-  renderer.update(performance.now());
-  requestAnimationFrame(update);
+  for (const beatmap of beatmaps) {
+    audios[0].play();
+    const renderer = await createRenderer({ beatmap });
+    document.getElementById("viewer_container")!.appendChild(renderer.canvas);
+    const update = () => {
+      console.log(audios[0].currentTime);
+      renderer.update(audios[0].currentTime * 1000);
+      requestAnimationFrame(update);
+    };
+    update();
+  }
 };
-update();
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <style>
+    #viewer_container {
+      display: flex;
+      gap: 10px;
+      flex-wrap: wrap;
+    }
+  </style>
   <div>
     <h1>osu! web replay viewer</h1>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
+    <button onclick="start()">Start</button>
+    <div id="viewer_container"></div>
   </div>
 `;
-document.getElementById("app")!.appendChild(renderer.canvas);
